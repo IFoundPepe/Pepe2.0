b7893bf2ce920da986a482c50579b97add0e5475
IFoundPepe <grwilson@gmail.com>
2017-05-06 11:27:12 -0700

    New Bluetooth example
    Added another example Bluetooth app to learn from.  There is a Readme
    file with the URL of the page I want to use as a lesson.

---
+++ BluetoothLeGatt README.txt
@@ -0,0 +1,3 @@
+Example for bluetooth LE app
+
+https://www.allaboutcircuits.com/projects/how-to-communicate-with-a-custom-ble-using-an-android-app/
--- PepeApp/app/src/main/java/com/buildthebird/pepeapp/MainActivity.java
+++ PepeApp/app/src/main/java/com/buildthebird/pepeapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.buildthebird.pepeapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +11,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void analogChanged(View view)
+    {
+        //read the "Look" and "Lean" progress bar, and send position value across bluetooth
+        //this would also be used to sample analog X/Y coordinates from controllers
+    }
+
 }
